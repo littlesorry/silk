b8d8d78bc5985d711ca0035a29530adec03a7496
littlesorry <chaumet.g@gmail.com>
2015-05-10 14:50:50 +0800

    add one data ut

--- test/data/dataTest.js
+++ test/data/dataTest.js
@@ -9,128 +9,29 @@ describe("data.js", function() {
         data("masterpiece")._clear();
     });
 
-
-
-    // describe("#find@user", function() {
-    //     it("should get no user", function(done) {
-    //         data("user").find({"coupon.code": "code001"}).then(function(docs) {
-    //             console.log(docs);
-    //             done();
-    //         });
-    //     });
-    // });
-
-    // describe("#insert@user", function() {
-    //     it("should insert user ok", function(done) {
-    //         data("user").insert({
-    //             openId: "open001",
-    //             coupon: {
-    //                 code: "code001"
-    //             }
-    //         }).then(function() {
-    //             done();
-    //         }).catch(function(err) {
-    //             done(err);
-    //         });
-    //     });
-    // });
-
-    // describe("#findOne@user", function() {
-    //     it("should get one user", function(done) {
-    //         data("user").findOne({"coupon.code": "code001"}).then(function(doc) {
-    //             doc.coupon.should.eql({
-    //                 code: "code001"
-    //             });
-    //             done();
-    //         });
-    //     });
-    // });
-
-    // describe("#insert@coupon", function() {
-    //     it("should insert coupon ok", function(done) {
-    //         data("coupon").insert({
-    //             code: "code001",
-    //             status: "unused"
-    //         }).then(function() {
-    //             done();
-    //         }).catch(function(err) {
-    //             done(err);
-    //         });
-    //     });
-    // });
-
-    describe("#findOneAndUpdate@coupon", function() {
-        it("should findOneAndUpdate one user", function(done) {
-            // data("shop").findOne({"code": "test2"}).then(function(doc) {
-            //     console.log(doc);
-            //     doc.update({$dec: {remainingNumber: -1}});
-            //     doc.save();
-            //     done();
-            // });
-            data("shop").findOne({"code": "test2"}).then(function(doc) {
-                console.log(doc);
-                data("shop").findOneAndUpdate({"code": "test2"}, {$inc: {remainingNumber: -1}})
-                .then(function(doc) {
-                    console.log(doc);
-                    done();
+    describe("#insert@masterpiece", function() {
+        var now = new Date();
+        it("should insert masterpiece ok", function(done) {
+            data("masterpiece").insert({
+                author: "unit test",
+                paths: [{x: 0, y: 0}, {x: 1, y: 1}],
+                createdAt: now
+            }).then(function(returned) {
+                data("masterpiece")
+                .findOne({author: "unit test"})
+                .then(function(actual) {
+                    actual.should.containEql({
+                        author: "unit test",
+                        createdAt: now
+                    });
+                    actual.should.have.property('paths').with.lengthOf(2);
+                    done();                        
                 });
-
-                
-
-                // doc.update({$dec: {remainingNumber: -1}});
-                // doc.save();
-            });        
+            }).catch(function(err) {
+                done(err);
+            });
         });
     });
 
-    // describe("#find", function() {
-    //     it("should get no target", function(done) {
-    //         data("shop").find({"state": "北京", "city": "北京"}).then(function(docs) {
-    //             docs.should.eql([]);
-    //             done();
-    //         });
-    //     });        
-    // });
-
-    // describe("#findOne", function() {
-    //     it("should get no target", function(done) {
-    //         data("shop").findOne({"state": "北京", "city": "北京"}).then(function(doc) {
-    //             done(doc);
-    //         });
-    //     });        
-    // });
-
-    // describe("#insert", function() {
-    //     it("should succeed", function(done) {
-    //         data("shop").insert({
-    //             id: "id000",
-    //             state: "北京",
-    //             city: "北京",
-    //             code: "aaa000"
-    //         }).then(function() {
-    //             done();
-    //         }).catch(function(err) {
-    //             done(err);
-    //         });
-    //     });     
-    // });
-
-    // describe("#find", function() {
-    //     it("should get 1 target", function(done) {
-    //         data("shop").find({"state": "北京", "city": "北京"}).then(function(docs) {
-    //             console.log(docs);
-    //             done();
-    //         });
-    //     });        
-    // });
-
-    // describe("#findOne", function() {
-    //     it("should get 1 target", function(done) {
-    //         data("shop").findOne({"state": "北京", "city": "北京"}).then(function(doc) {
-    //             console.log(doc);
-    //             done();
-    //         });
-    //     });        
-    // });
 })
 
